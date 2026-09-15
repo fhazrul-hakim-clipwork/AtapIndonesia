9885e7106ac58ab176d4a9b08148a0bfadb1c4ed
Fadilla Apriyadi <fadillaapriyadi19@gmail.com>
2026-09-15 15:52:17 +0700

    fix compiled view path for Railway

---
+++ config/view.php
@@ -0,0 +1,30 @@
+<?php
+
+return [
+
+    /*
+    |--------------------------------------------------------------------------
+    | View Paths
+    |--------------------------------------------------------------------------
+    |
+    | The paths that should be used to locate Blade views.
+    |
+    */
+
+    'paths' => [
+        resource_path('views'),
+    ],
+
+    /*
+    |--------------------------------------------------------------------------
+    | Compiled View Path
+    |--------------------------------------------------------------------------
+    |
+    | This option determines where the compiled Blade templates will be
+    | stored.
+    |
+    */
+
+    'compiled' => storage_path('framework/views'),
+
+];
